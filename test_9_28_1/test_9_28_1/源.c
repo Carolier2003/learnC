b8c5362d3989a547cdbdd4ae742b1638a5a4eb0b
Galer <2398269504@qq.com>
2022-09-28 14:51:17 +0000

    test
    Signed-off-by: Galer <2398269504@qq.com>

---
+++ test_9_28_1/test_9_28_1/源.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() 
+{
+	double x, result;
+	scanf_s("%lf",&x);
+	if (x == 0)
+		result = 0;
+	else
+		result = 1 / x;
+	printf("f(%.1f) = %.1f", x, result);
+	return 0;
+}
