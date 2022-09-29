f635ad27d05c5a660b3433d8aba999d94081020b
Galer <2398269504@qq.com>
2022-09-29 08:09:10 +0000

    test
    Signed-off-by: Galer <2398269504@qq.com>

---
+++ test_9_29_2/test_9_29_2/test_9_29_2/源.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int lower, upper, fahr;
+	double celsius;
+	scanf_s("%d%d", &lower, &upper);
+	if (lower <= upper)
+	{
+		printf("fahr celsius\n");
+		for (fahr = lower; fahr <= upper; fahr = fahr + 2)
+		{
+			celsius = 5.0 * (fahr - 32) / 9.0;
+			printf("%d%6.1lf\n", fahr, celsius);
+		}
+	}
+	else
+		printf("Invalid.");
+	return 0;
+}
+
