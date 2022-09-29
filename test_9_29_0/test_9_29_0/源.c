956af1a42cdfee637349010a6a419561b6be040c
Galer <2398269504@qq.com>
2022-09-29 03:56:33 +0000

    test
    Signed-off-by: Galer <2398269504@qq.com>

---
+++ test_9_29_0/test_9_29_0/源.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	double x, y;
+	scanf_s("%lf", &x);
+	if (x < 0)
+		y = pow((x + 1), 2) + 2 * x + 1 / x;
+	else
+		y = sqrt(x);
+	printf("f(%.2f) = %.2f", x, y);
+}
